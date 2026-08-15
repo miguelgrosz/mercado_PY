eb764c55111b1470db1762bd0e461c1a05baf3b5
Miguel araujo grosz <miguelgrosz12@gmail.com>
2026-08-15 18:54:23 -0300

    Falta poder ediata bbanco

--- conexao/salvar_cadastro.py
+++ conexao/salvar_cadastro.py
@@ -1,102 +1,130 @@
-
-# Instala as bibliotecas necessárias:
-# fastapi  -> cria a API
-# uvicorn  -> servidor que executa a API
-# pymysql  -> permite o Python conversar com o MySQL
-# python-multipart -> permite receber dados enviados por formulário HTML
-
-# Esse comando é executado no terminal, não dentro do código:
-# pip install fastapi uvicorn pymysql python-multipart
-
-
-# Importa o FastAPI para criar a API
-# Importa Form para receber dados vindos de um formulário HTML
-from fastapi import FastAPI, Form
-
-
-# Importa a biblioteca que conecta o Python ao MySQL
+from fastapi import FastAPI, Form, Path #Response
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 import pymysql
 
 
-# Cria a aplicação FastAPI
-# O objeto "app" é o servidor da API
 app = FastAPI()
 
 
-# Cria uma conexão com o banco de dados MySQL
+# Libera o HTML acessar a API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Conexão MySQL
 conexao = pymysql.connect(
-
-    # Endereço do banco
-    # localhost significa que o MySQL está no próprio computador
     host="localhost",
-
-    # Usuário do MySQL
     user="root",
-
-    # Senha do usuário MySQL
     password="root",
-
-    # Banco que será utilizado
     database="cadastro"
 )
 
 
-# Cria uma rota da API
-# Quando alguém enviar um POST para /cadastro,
-# essa função será executada
+# CADASTRO
 @app.post("/cadastro")
-
-
-# Função que recebe os dados enviados pelo HTML
 def cadastrar(
-
-    # Pega o campo "nome" do formulário
-    # str significa que esperamos um texto
-    # Form(...) significa que vem de um formulário
-    # ... significa que é obrigatório
     nome: str = Form(...),
-
-
-    # Pega o campo "senha" do formulário
     senha: str = Form(...),
-
-
-    # Pega o campo "email" do formulário
     email: str = Form(...)
-
 ):
 
-
-    # Cria um cursor.
-    # O cursor é usado para mandar comandos SQL para o banco
     cursor = conexao.cursor()
 
-
-    # Comando SQL que será executado
-    #
-    # O %s é um espaço reservado.
-    # Depois ele será substituído pelos valores reais.
     sql = """
     INSERT INTO usuarios (nome, senha, email)
     VALUES (%s, %s, %s)
     """
 
-
-    # Executa o comando SQL
-    #
-    # Aqui o Python troca:
-    #
-    # primeiro %s -> nome
-    # segundo %s -> senha
-    # terceiro %s -> email
-    #
     cursor.execute(sql, (nome, senha, email))
 
-
-    # Confirma a alteração no banco.
-    # Sem o commit, o INSERT pode não ser salvo.
     conexao.commit()
 
+    cursor.close()
 
-    # Retorna uma resposta para quem chamou a API
-    return {"mensagem": "Usuário cadastrado!"}
+    return RedirectResponse(
+        url="http://127.0.0.1:5500/mercado_PY/frontend/pagina_login.html",
+        status_code=303
+    )
+
+
+
+# LOGIN
+@app.post("/login")
+def login(
+    nome: str = Form(...),
+    senha: str = Form(...)
+):
+
+    cursor = conexao.cursor()
+
+    sql = """
+    SELECT * FROM usuarios
+    WHERE nome=%s AND senha=%s
+    """
+
+    cursor.execute(sql, (nome, senha))
+
+    usuario = cursor.fetchone()
+
+    cursor.close()
+
+
+    if usuario:
+        return RedirectResponse(
+            url="http://127.0.0.1:5500/mercado_PY/frontend/pagina_log.html",
+            status_code=303
+        )
+
+    else:
+        return {
+            "mensagem": "Nome ou senha incorretos"
+        }
+
+
+
+# MOSTRAR USUÁRIOS
+@app.get("/usuarios")
+def listar_usuarios():
+
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        "SELECT id, nome, senha, email FROM usuarios"
+    )
+
+    usuarios = cursor.fetchall()
+
+    cursor.close()
+
+    return usuarios
+
+@app.delete("/usuarios/{id}")
+def excluir_usuario(id: int):
+
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        "DELETE FROM usuarios WHERE id = %s",
+        (id,)
+    )
+
+    conexao.commit()
+
+    cursor.close()
+
+    return {"mensagem": "Usuário excluído com sucesso"}
+
+
+# @app.get("/paginadinamica/{caminho}")
+# def paginaDinamica(response: Response):
+#     txt = "<html><body><h1>Hellow</h1></body></html>"        
+#     return Response(
+#         content=txt, 
+#         status_code=200, 
+#         headers={"Content-Type": "text/html; charset=utf-8"}
+#     )
